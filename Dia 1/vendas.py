f897d417648737c9954b31c62bb87d44b0a98145
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-02-09 16:09:19 -0300

    Agrupando colunas

--- Dia 1/vendas.py
+++ Dia 1/vendas.py
@@ -3,12 +3,14 @@ import pandas as pd
 
 if __name__ == '__main__':
     tabela_vendas = pd.read_excel("Vendas.xlsx")  # importando base de dados
-    display(tabela_vendas)  # mostrando base de dados
 
     """
     Realizando o cálculo do faturamento das lojas
     """
+    tabela_filtrada = tabela_vendas[["ID Loja", "Valor Final"]]
+    # filtramos a tabela, pegando apenas essa duas colunas
 
-    tabela_faturamento = tabela_vendas[["ID Loja", "Valor Final"]]  # filtramos a tabela, pegando apenas
-    # essa duas colunas
+    tabela_faturamento = tabela_filtrada.groupby("ID Loja").sum()
+    # Agrupamos as lojas pelo nome, e somamos a coluna de valor final
+
     display(tabela_faturamento)
