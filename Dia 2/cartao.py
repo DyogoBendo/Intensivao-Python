1f1547f05753d3c6639bdce22f581a010ee7d8a9
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-02-10 17:13:01 -0300

    Criando gráficos

--- Dia 2/cartao.py
+++ Dia 2/cartao.py
@@ -1,5 +1,16 @@
 import pandas as pd
+import plotly.express as px
 from IPython.display import display
+
+
+def gera_grafico_coluna(coluna, tabela):
+    fig = px.histogram(tabela, x=coluna, color="Categoria")
+    # geramos um grafico de histograma, em que o eixo x é definido pela coluna passada
+    # e as cores são baseadas na quantidade de cada categoria, que corresponde aquela coluna
+
+    fig.show()
+    # mostramos a figura
+
 
 if __name__ == '__main__':
 
@@ -19,9 +30,10 @@ if __name__ == '__main__':
     clients_df_categoria = clients_df["Categoria"].value_counts()
     # contamos o número de cada elemento presente na coluna categoria
 
-    display(clients_df_categoria)
-
     clients_df_categoria_pct = clients_df["Categoria"].value_counts(normalize=True)
     # pegamos agora o percentual de presença de cada elemento
 
-    display(clients_df_categoria_pct)
+    for coluna in clients_df:  # automaticamente, em um for, pegamos as colunas da tabela
+        gera_grafico_coluna(coluna, clients_df)
+
+
